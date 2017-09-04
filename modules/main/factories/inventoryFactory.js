c71e15a1b3c649a0f4a3e45d77b79c95254bf878
Nicole Francisco <nfrancisco@Nicoles-MacBook-Pro.local>
2017-09-04 18:32:39 -0400

    Enhances readability and prepares for possibility of having many more kinds of inventory items.

--- modules/main/factories/inventoryFactory.js
+++ modules/main/factories/inventoryFactory.js
@@ -1,17 +1,14 @@
 (function() {
     var mainModule = angular.module("mainModule");
-    mainModule.factory("InventoryFactory", function itemsFactory(InventoryItemsFactory) {
-
-        var agedBrieConfig = {
+    mainModule.factory("InventoryFactory", function itemsFactory(_, InventoryItemsFactory) {
+        var configurationList = [{
             name: "Aged Brie",
             qualityValue: {
                 enhanceBy: function enhanceBy() {
                     return 1;
                 }
             }
-        };
-
-        var sulfurasConfig = {
+        }, {
             name: "Sulfuras the Legendary Sword",
             isLegendary: true,
             minSellIn: 1, //Because this item "never has to be sold", sell-in value should never reach 0
@@ -20,9 +17,7 @@
                     return 0;
                 }
             }
-        };
-
-        var backstagePassConfig = {
+        }, {
             name: "Backstage Pass",
             qualityValue: {
                 enhanceBy: function enhanceBy(sellInValue) {
@@ -45,24 +40,16 @@
                     return result;
                 }
             }
-        };
-
-        var conjuredShieldConfig = {
+        }, {
             name: "Conjured Shield",
             degradationFactorRelativeToNormal: 2
-        };
-
-        var regularShieldConfig = {
+        }, {
             name: "Regular Shield",
-        };
+        }];
 
-        var agedBrie = InventoryItemsFactory.create(agedBrieConfig);
-        var sulfuras = InventoryItemsFactory.create(sulfurasConfig);
-        var backstagePass = InventoryItemsFactory.create(backstagePassConfig);
-        var conjuredShield = InventoryItemsFactory.create(conjuredShieldConfig);
-        var regulardShield = InventoryItemsFactory.create(regularShieldConfig);
-
-        var inventory = [agedBrie, sulfuras, backstagePass, conjuredShield, regulardShield];
+        var inventory = _.map(configurationList, function createInstance(configuration) {
+            return InventoryItemsFactory.create(configuration);
+        })
 
         return {
             inventory: inventory
