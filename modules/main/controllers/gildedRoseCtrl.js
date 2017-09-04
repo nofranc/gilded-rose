e8b5a10cdff817e06a5408c5d4098836b7f80d83
Nicole Francisco <nfrancisco@Nicoles-MacBook-Pro.local>
2017-09-04 18:03:51 -0400

    Creates table of inventory items and update & reset buttons.

--- modules/main/controllers/gildedRoseCtrl.js
+++ modules/main/controllers/gildedRoseCtrl.js
@@ -1,6 +1,22 @@
 (function() {
     var mainModule = angular.module("mainModule");
-    mainModule.controller("MainCtrl", function(InventoryItemsFactory) {
+    mainModule.controller("MainCtrl", function(_, InventoryFactory) {
         var vm = this;
+
+        vm.inventory = InventoryFactory.inventory;
+        vm.updateAll = updateAll;
+        vm.reset = reset;
+
+        function updateAll() {
+            _.forEach(InventoryFactory.inventory, function update(inventoryItem) {
+                inventoryItem.update();
+            });
+        }
+
+        function reset() {
+            _.forEach(InventoryFactory.inventory, function update(inventoryItem) {
+                inventoryItem.resetValues();
+            });
+        }
     });
 })();
